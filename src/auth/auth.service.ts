5ccfe543bf93eeaa7cd7bed54c0dddb3e8e7a777
jose <josejr017@gmail.com>
2025-09-04 14:11:32 -0300

    feat: method to create jwt access token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
+import { UsersService } from 'src/users/users.service';
+import { HashingService } from 'src/commom/hashing/hashing.service';
+import { JwtPayload } from './types/jwt-payload.type';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  doLogin(loginDto: LoginDto) {
-    return loginDto;
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly hashingService: HashingService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+
+    // Buscar user por E-mail
+    const user = await this.usersService.findByEmail(email);
+    const error = new UnauthorizedException('Usuário ou senha inválida');
+
+    if (!user) throw error;
+
+    // Comparar senha digitada com a do BD
+    const isPasswordValid = await this.hashingService.compare(
+      password,
+      user.password,
+    );
+
+    if (!isPasswordValid) throw error;
+
+    const jwtPayload: JwtPayload = {
+      sub: user.id,
+      email: user.email,
+    };
+
+    const acessToken = await this.jwtService.signAsync(jwtPayload);
+
+    user.forceLogout = false;
+    await this.usersService.save(user);
+
+    return { acessToken };
   }
 }
