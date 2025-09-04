455b9f56540dc56c62043f35da4321c0b36a2709
jose <josejr017@gmail.com>
2025-09-04 20:06:42 -0300

    feat: post entity and Dtos (create, update and response)

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,1 +1,29 @@
-export class CreatePostDto {}
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Length,
+} from 'class-validator';
+
+export class CreatePostDto {
+  @IsString({ message: 'O título precisa ser uma string' })
+  @Length(10, 150, {
+    message: 'O título precisa ter entre 10 e 150 caracteres',
+  })
+  title: string;
+
+  @IsString({ message: 'O excerto precisa ser uma string' })
+  @Length(10, 200, {
+    message: 'O excerto precisa ter entre 10 e 200 caracteres',
+  })
+  excerpt: string;
+
+  @IsString({ message: 'O conteúdo precisa ser uma string' })
+  @IsNotEmpty({ message: 'O conteúdo não pode ficar vazio' })
+  content: string;
+
+  @IsOptional()
+  @IsUrl({ require_tld: false }) // Top level domain proíbe localhost e IP
+  coverImageUrl?: string;
+}
--- src/post/entities/post.entity.ts
+++ src/post/entities/post.entity.ts
@@ -1,1 +1,44 @@
-export class Post {}
+import { User } from 'src/user/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('posts')
+export class Post {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column({ unique: true })
+  slug: string;
+
+  @Column('text')
+  content: string;
+
+  @Column()
+  excerpt: string;
+
+  @Column({ nullable: true })
+  coverImageUrl: string;
+
+  @Column({ default: false })
+  published: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToOne(() => User)
+  author: User;
+
+  // pretendo posteriormente adicionar category e club
+}
