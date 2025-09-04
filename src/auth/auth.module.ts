b77b589af1be2b8f003ca1345c893d6ec258035d
jose <josejr017@gmail.com>
2025-09-04 14:09:44 -0300

    feat: configuring the JwtModule to be used only in the AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,30 @@
-import { Module } from '@nestjs/common';
+import { InternalServerErrorException, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from 'src/users/users.module';
+import { CommomModule } from 'src/commom/commom.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    CommomModule,
+    JwtModule.registerAsync({
+      useFactory: () => {
+        const secret = process.env.JWT_SECRET;
+
+        if (!secret)
+          throw new InternalServerErrorException(
+            'JWT_SECRET não encontrada no .env',
+          );
+
+        return {
+          secret,
+          signOptions: { expiresIn: process.env.JWT_EXPIRATION || '1d' },
+        };
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [],
