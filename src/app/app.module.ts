228535c50e76f237115a314a361c1d5e093de5cd
jose <josejr017@gmail.com>
2025-09-04 20:19:05 -0300

    refactor: refactoring app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { PostModule } from './post/post.module';
+import { AuthModule } from '../auth/auth.module';
+import { UserModule } from '../user/user.module';
+import { PostModule } from '../post/post.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
@@ -30,7 +28,5 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       },
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
