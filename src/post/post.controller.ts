85aaffbf1bb85cdcd74b256d33cd35a5a2385784
jose <josejr017@gmail.com>
2025-09-06 12:20:44 -0300

    feat: endpoint to create Post

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -6,18 +6,26 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Req,
 } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import type { AuthenticatedRequest } from 'src/auth/types/authenticated-request';
 
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
-  create(@Body() createPostDto: CreatePostDto) {
-    return this.postService.create(createPostDto);
+  create(
+    @Body() createPostDto: CreatePostDto,
+    @Req() req: AuthenticatedRequest,
+  ) {
+    return this.postService.create(createPostDto, req.user);
   }
 
   @Get()
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,11 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { User } from 'src/user/entities/user.entity';
+import { Repository } from 'typeorm';
+import { Post } from './entities/post.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PostResponseDto } from './dto/post-response.dto';
+import { createSlugFromText } from 'src/commom/utils/create-slug-from-text';
 
 @Injectable()
 export class PostService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+  private readonly logger = new Logger(PostService.name);
+
+  constructor(
+    @InjectRepository(Post)
+    private readonly postRepository: Repository<Post>,
+  ) {}
+
+  async create(createPostDto: CreatePostDto, author: User) {
+    const { title, excerpt, content, coverImageUrl } = createPostDto;
+
+    const newPost = this.postRepository.create({
+      author,
+      content,
+      excerpt,
+      coverImageUrl,
+      slug: createSlugFromText(title),
+      title,
+    });
+
+    const postCreated = await this.save(
+      this.postRepository.create(newPost),
+    ).catch((e: unknown) => {
+      if (e instanceof Error)
+        this.logger.error('Erro ao criar o post', e.stack);
+
+      throw new BadRequestException('Erro ao criar o post');
+    });
+
+    return new PostResponseDto(postCreated);
   }
 
   findAll() {
@@ -22,5 +55,10 @@ export class PostService {
 
   remove(id: number) {
     return `This action removes a #${id} post`;
+  }
+
+  // Métodos auxiliares
+  save(post: Post) {
+    return this.postRepository.save(post);
   }
 }
